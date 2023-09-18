73dbd046fbf21ae8973721678232984f51f86e11
Jamiu Olanrewaju <96052702+techwithlanre@users.noreply.github.com>
2023-09-18 11:41:56 +0100

    Tuesday 18th Completed

--- create-student.php
+++ create-student.php
@@ -1,1 +1,77 @@
 <?php
+
+$courses_query = $db->prepare('SELECT * FROM courses');
+$result = $courses_query->execute();
+$courses_list = $courses_query->fetchAll();
+
+$sessions_query = $db->prepare('SELECT * FROM sessions');
+$result = $sessions_query->execute();
+$sessions_list = $sessions_query->fetchAll();
+
+if (isset($_POST['first_name'])) {
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $gender = $_POST['gender'];
+    $address = $_POST['address'];
+    $course_id = $_POST['course_id'];
+    $session_id = $_POST['session_id'];
+    $reg_date = date('Y-m-d');
+
+
+    $insert_query = $db->prepare('INSERT INTO students SET first_name = ?, last_name = ?, email = ?, 
+                         phone = ?, gender = ?, address = ?, course_id = ?, session_id = ?, reg_date = ?');
+    $result = $insert_query->execute([$first_name, $last_name,$email, $phone, $gender, $address, $course_id, $session_id, $reg_date]);
+
+}
+
+?>
+
+
+<div>
+    <div class="w3-container w3-teal" style="margin-top: 50px;">
+        <h2>Create New Student</h2>
+    </div>
+
+    <form class="w3-container" method="post" action="<?= $_SERVER['PHP_SELF']. '?view=create-student'; ?>">
+        <label class="w3-text-teal"><b>Name</b></label>
+        <input class="w3-input w3-border w3-light-grey" name="first_name" type="text" required>
+
+        <label class="w3-text-teal"><b>Name</b></label>
+        <input class="w3-input w3-border w3-light-grey" name="last_name" type="text" required>
+
+        <label class="w3-text-teal"><b>Phone</b></label>
+        <input class="w3-input w3-border w3-light-grey" name="phone" type="text" required>
+
+        <label class="w3-text-teal"><b>Email</b></label>
+        <input class="w3-input w3-border w3-light-grey" name="email" type="text" required>
+
+        <label class="w3-text-teal"><b>Address</b></label>
+        <input class="w3-input w3-border w3-light-grey" name="address" type="text" required>
+
+        <label class="w3-text-teal"><b>Gender</b></label>
+        <select class="w3-input w3-border w3-light-grey" name="gender" type="text" required>
+            <option disabled selected value="">Select Gender</option>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+        </select>
+
+        <select class="w3-input w3-border w3-light-grey" name="course_id" type="text" required>
+            <option disabled selected value="">Select Course</option>
+            <?php foreach ($courses_list as $course): ?>
+                <option value="<?= $course['id'] ?>"><?= $course['name'] ?></option>
+            <?php endforeach; ?>
+        </select>
+
+        <select class="w3-input w3-border w3-light-grey" name="session_id" type="text" required>
+            <option disabled selected value="">Select Session</option>
+            <?php  foreach ($sessions_list as $session) { ?>
+                <option value="<?= $session['id'] ?>"><?= $session['name'] ?></option>
+            <?php } ?>
+        </select>
+
+        <button class="w3-btn w3-blue-grey" style="margin-top: 30px;">CREATE USER</button>
+    </form>
+</div>
+
--- students.php
+++ students.php
@@ -1,30 +1,61 @@
 <?php
 
+$students_query = $db->prepare('SELECT * FROM students');
+$result = $students_query->execute();
+$studentCount = $students_query->rowCount();
+$studentList = $students_query->fetchAll();
+
+
+function get_course_name ($id) {
+    global $db;
+    $course_query = $db->prepare('SELECT * FROM courses WHERE id = ? ');
+    $result = $course_query->execute([$id]);
+    $course = $course_query->fetch();
+    return $course['name'];
+}
+
+function get_session_name($id) {
+    global $db;
+    $sessions_query = $db->prepare('SELECT * FROM sessions WHERE id = ? ');
+    $result = $sessions_query->execute([$id]);
+    $session = $sessions_query->fetch();
+    return $session['name'];
+
+}
+
 ?>
 
 <div>
     <h2>Students List</h2>
     <p>This table displays a list of students</p>
 
-    <a href="index.php?view=create-user" style="margin-bottom: 50px;">Create new student</a>
+    <a href="index.php?view=create-student" style="margin-bottom: 50px;">Create new student</a>
 
-    <?php if ($userCount > 0): ?>
+    <?php if ($studentCount > 0): ?>
         <table class="w3-table w3-striped">
             <thead>
             <tr>
-                <th>Name</th>
+                <th>First Name</th>
+                <th>Last Name</th>
                 <th>Phone</th>
                 <th>Email</th>
                 <th>Gender</th>
+                <th>Address</th>
+                <th>Session</th>
+                <th>Course</th>
             </tr>
             </thead>
             <tbody>
-            <?php foreach ($result as $user): ?>
+            <?php foreach ($studentList as $student): ?>
                 <tr>
-                    <td><?= $user['name'] ?></td>
-                    <td><?= $user['phone'] ?></td>
-                    <td><?= $user['email'] ?></td>
-                    <td><?= $user['gender'] ?></td>
+                    <td><?= $student['first_name'] ?></td>
+                    <td><?= $student['last_name'] ?></td>
+                    <td><?= $student['phone'] ?></td>
+                    <td><?= $student['email'] ?></td>
+                    <td><?= $student['gender'] ?></td>
+                    <td><?= $student['address'] ?></td>
+                    <td><?= get_session_name($student['session_id']) ?></td>
+                    <td><?= get_course_name($student['course_id']) ?></td>
                 </tr>
             <?php endforeach; ?>
             </tbody>
